af0df6730d65dfa7d12082570c29ca3e243783dd
unknown <amedina@growthaccelerationpartners.com>
2020-02-11 13:19:58 -0500

    Add body parser. Add post method.

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
+const bodyParser = require('body-parser')
 const express = require('express')
 
 const app = express()
 
+const calcs = ['1']
+
+app.use(bodyParser)
+
+app.get('/api/calcs', (_, res) => {
+  res.json(calcs)
+})
+
 app.get('*', (req, res) => {
   res.send('hello universe')
+})
+
+app.post('/github/test', (req, res) => {
+  console.log(req.body)
 })
 
 app.listen('8080', () => {
